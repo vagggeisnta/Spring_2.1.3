cb6d722d4e33c5a62767e6f8e8322c8ffb53de80
Vage <lukjanovvalera@gmail.com>
2020-04-03 19:09:36 +0300

    first

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,15 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
